423c0c8e3e4f10d34d01433316f3947865dcd5e3
Prasheel <prasheelsoni11@gmail.com>
2015-11-05 19:41:13 +0530

    Delete Locations Added(Modified)

--- app/src/main/java/com/neoresearch/bookmyspace/PageAdapterPP.java
+++ app/src/main/java/com/neoresearch/bookmyspace/PageAdapterPP.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class PageAdapterPP extends FragmentPagerAdapter {
 
 
-        final int PAGE_COUNTPP = 2;
+        final int PAGE_COUNTPP = 3;
 
         public PageAdapterPP(FragmentManager fm) {
 
@@ -31,16 +31,21 @@ public class PageAdapterPP extends FragmentPagerAdapter {
             switch (position) {
                 case 0:
                     Locations loc = new Locations();
+
                      return loc;
+
                   //  Login log=new Login();
                   //  return log;
 
                 case 1:
                     AddLocations ad=new AddLocations();
                     return ad;
+
                    // Register r=new Register();
                    // return r;
-
+                case 2:
+                    LocationsMap lm=new LocationsMap();
+                    return lm;
 
             }
             return null;
