cf33cc02ad8ebd1307f92547fdb1363486138ea9
Prasheel <prasheelsoni11@gmail.com>
2015-11-06 23:22:51 +0530

    GPS Coordinates

--- app/src/main/java/com/neoresearch/bookmyspace/AddLocations.java
+++ app/src/main/java/com/neoresearch/bookmyspace/AddLocations.java
@@ -1,5 +1,6 @@
     package com.neoresearch.bookmyspace;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.ContentResolver;
@@ -7,6 +8,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Address;
+import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
@@ -34,38 +36,38 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
-    public class AddLocations extends Fragment implements View.OnClickListener {
+    public class AddLocations extends Fragment implements View.OnClickListener,LocationListener {
 
         EditText location_name, h_no, street, area, space;
         protected ProgressDialog pDialog;
         ImageButton redirect;
-        String mobile, loc_name, house, street_n, area_n, space_n, longitude, latitude;
-        private LocationManager locationMangaer = null;
-        private LocationListener locationListener = null;
-        private static final String TAG = "Debug";
-        private Boolean flag = false;
+        String mobile, loc_name, house, street_n, area_n, space_n, longitude, latitude,provider;
         SessionManager session;
         Double lati,longi;
         JSONParser jsonParser = new JSONParser();
         Button add;
-        GPSTracker gps;
-
 
         private static final String LOCATION_URL = "http://prasheel-acropolis.rhcloud.com/add_locations.php";
         private static final String TAG_SUCCESS = "success";
+        LocationManager locationManager;
 
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            Context c = getActivity().getApplicationContext();
+            locationManager =(LocationManager)activity.getSystemService(Context.LOCATION_SERVICE);
+
+        }
 
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View v = inflater.inflate(R.layout.addlocations, container, false);
             location_name = (EditText) v.findViewById(R.id.location_name);
-
             h_no = (EditText) v.findViewById(R.id.home_number);
             space = (EditText) v.findViewById(R.id.space);
             street = (EditText) v.findViewById(R.id.street);
             area = (EditText) v.findViewById(R.id.area);
             add = (Button) v.findViewById(R.id.add_locations);
-            locationMangaer = (LocationManager)
-                    getActivity().getSystemService(Context.LOCATION_SERVICE);
             session = new SessionManager(getActivity().getApplicationContext());
             HashMap<String, String> user = session.getUserDetails();
             redirect = (ImageButton) v.findViewById(R.id.imageButton4);
@@ -73,8 +75,31 @@ import java.util.Locale;
             add.setOnClickListener(this);
             mobile = user.get(SessionManager.KEY_MOBILE);
 
+            Criteria criteria = new Criteria();
+            provider = locationManager.getBestProvider(criteria, false);
+            Location location = null;
+            Toast.makeText(getActivity(), "Getting your GPS Location", Toast.LENGTH_LONG).show();
+
+            if (location != null) {
+                System.out.println("Provider " + provider + " has been selected.");
+                onLocationChanged(location);
+            } else {
+
+            }
 
             return v;
+
+        }
+        @Override
+        public void onResume() {
+            super.onResume();
+            locationManager.requestLocationUpdates(provider, 400, 1, this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            locationManager.removeUpdates(this);
         }
 
 
@@ -96,41 +121,44 @@ import java.util.Locale;
             }
         }
 
+        @Override
+        public void onLocationChanged(Location location) {
+
+            lati =  location.getLatitude();
+             longi = location.getLongitude();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+
 
         class AddLocation extends AsyncTask<String, String, String> {
 
             @Override
             protected void onPreExecute() {
-
                 pDialog = new ProgressDialog(getActivity());
                 pDialog.setMessage("Adding Location...");
                 pDialog.setIndeterminate(false);
                 pDialog.setCancelable(true);
                 pDialog.show();
-
-
             }
-
-
-
-
         @Override
         protected String doInBackground(String... args) {
             int success;
-
-
-            gps = new GPSTracker(getActivity());
-            if(gps.canGetLocation()){
-                lati=gps.getLatitude(); // returns latitude
-                longi=gps.getLongitude(); // returns longitude
-            }
-            else{gps.showSettingsAlert();}
-
-                longitude=longi.toString();
-                latitude=lati.toString();
-
-
-
+           latitude=lati.toString();
+            longitude=longi.toString();
             try {
                 // Building Parameters
                 List<NameValuePair> params = new ArrayList<>();
@@ -145,10 +173,13 @@ import java.util.Locale;
 
 
                 Log.d("request!", "starting");
+
                 // getting product details by making HTTP request
                 JSONObject json = jsonParser.makeHttpRequest(LOCATION_URL, "POST", params);
+
                 // check your log for json response
                 Log.d("Sending Mobile", json.toString());
+
                 // json success tag
                 success = json.getInt(TAG_SUCCESS);
 
@@ -156,13 +187,13 @@ import java.util.Locale;
                 if (success == 1) {
                     Toast.makeText(getActivity().getApplicationContext(), "Locations Added!!!", Toast.LENGTH_LONG).show();
                     Log.d("Locations Added", json.toString());
-                    //***CODE HERE TO SHOW LOCATIONS*****
+
 
                     return json.getString(TAG_SUCCESS);
                 } else {
                     Toast.makeText(getActivity().getApplicationContext(), "Error Occured!!!", Toast.LENGTH_LONG).show();
                     Log.d("Error Occured", json.getString(TAG_SUCCESS));
-                    //*****CODE HERE TO SHOW EMPTY SCREEN********
+
 
                     return json.getString(TAG_SUCCESS);
                 }
@@ -175,7 +206,7 @@ import java.util.Locale;
         @Override
         protected void onPostExecute(String file_url) {
             pDialog.dismiss();
-            gps.showSettingsAlert();
+
 
             if (file_url != null) {
                 Toast.makeText(getActivity().getApplicationContext(), file_url, Toast.LENGTH_LONG).show();
